f4507f1bd356bb55dda98f0dcc3916af4e358001
KaranGRohit <karanceddu@gmail.com>
2019-02-05 22:52:15 +0530

    adding new model in accountant
    Signed-off-by: KaranGRohit <karanceddu@gmail.com>

--- Accountant_App/admin.py
+++ Accountant_App/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import Accountant
+from .models import Accountant, Salary, Fees
 
 admin.site.register(Accountant)
+admin.site.register(Salary)
+admin.site.register(Fees)
