5925fa316ca7bba714a0eb2739c2daf79da90495
KaranGRohit <karanceddu@gmail.com>
2019-02-05 22:43:42 +0530

    adding new model in accountant
    Signed-off-by: KaranGRohit <karanceddu@gmail.com>

--- Accountant_App/models.py
+++ Accountant_App/models.py
@@ -6,10 +6,24 @@ from Student_App.models import Student
 from Teacher_App.models import Teacher
 
 class Accountant(models.Model):
-    ATid = models.ForeignKey(Teacher, models.SET_NULL, blank=True, null=True)
     Aid = models.IntegerField()
     AFname = models.CharField(max_length=20)
     ALname = models.CharField(max_length=20)
     Email = models.EmailField()
-    salary = models.IntegerField()
+
+
+class Salary(models.Model):
+    ATid = models.ForeignKey(Teacher, models.SET_NULL, blank=True, null=True)
+    exp = models.IntegerField()
+    amnt = models.IntegerField()
+    sal_date = models.DateField()
+
+class Fees(models.Model):
+    ASId = models.ForeignKey(Student, models.SET_NULL, blank=True, null=True)
     fee = models.IntegerField()
+    last_date = models.DateField()
+    due_charge = models.IntegerField()
+
+
+
+
