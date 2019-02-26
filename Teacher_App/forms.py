655d6c9781af71817b507dfdb8e1b62d7865e98a
KaranGRohit <karanceddu@gmail.com>
2019-02-26 17:46:03 +0530

    changes in  new views
    Signed-off-by: KaranGRohit <karanceddu@gmail.com>

--- Teacher_App/forms.py
+++ Teacher_App/forms.py
@@ -47,12 +47,3 @@ class ResultForm(forms.Form):
 
 
 
-Rid = models.IntegerField()
-grade = models.CharField(max_length=4)
-sub1 = models.CharField(max_length=20)
-sub2 = models.CharField(max_length=20)
-sub3 = models.CharField(max_length=20)
-marks1 = models.IntegerField()
-marks2 = models.IntegerField()
-marks3 = models.IntegerField()
-
