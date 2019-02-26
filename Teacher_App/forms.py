56a102778869b73ca36ffe7e111f85ddb35cce64
KaranGRohit <karanceddu@gmail.com>
2019-02-26 17:45:06 +0530

    changes in  new views to student app timetable view
    Signed-off-by: KaranGRohit <karanceddu@gmail.com>

--- Teacher_App/forms.py
+++ Teacher_App/forms.py
@@ -7,25 +7,40 @@ class ResultForm(forms.Form):
         'class':'form-control',
         'placeholder':'Rid',
     }))
+
     grade = forms.CharField(widget=forms.TextInput(attrs={
         'class': 'form-control',
         'placeholder': 'grade',
     }))
+
     sub1 = forms.CharField(widget=forms.TextInput(attrs={
         'class': 'form-control',
         'placeholder': 'sub1',
     }))
+
+    marks1 = forms.CharField(widget=forms.TextInput(attrs={
+        'class': 'form-control',
+        'placeholder': 'marks1',
+    }))
+
     sub2 = forms.CharField(widget=forms.TextInput(attrs={
         'class': 'form-control',
         'placeholder': 'sub2',
     }))
+
+    marks2 = forms.CharField(widget=forms.TextInput(attrs={
+        'class': 'form-control',
+        'placeholder': 'marks2',
+    }))
+
     sub3 = forms.CharField(widget=forms.TextInput(attrs={
         'class': 'form-control',
         'placeholder': 'sub3',
     }))
-    grade = forms.CharField(widget=forms.TextInput(attrs={
+
+    marks3 = forms.CharField(widget=forms.TextInput(attrs={
         'class': 'form-control',
-        'placeholder': 'grade',
+        'placeholder': 'marks3',
     }))
 
 
