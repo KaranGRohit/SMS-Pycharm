abded3aabbe6aacb3c014da7b5f348e1d9045b35
KaranGRohit <karanceddu@gmail.com>
2019-02-26 19:48:09 +0530

    new changes in form of result html
    Signed-off-by: KaranGRohit <karanceddu@gmail.com>

--- Teacher_App/views.py
+++ Teacher_App/views.py
@@ -21,24 +21,12 @@ def uploadresult(request):
             new_req.save()
             return redirect('viewresult')
 
+    else:
+        form = ResultForm()
+        ctx = {'form': form}
 
+    render(request,'uploadresult.html',ctx)
 
-
-
-
-    Roll_no = models.ForeignKey(Student,on_delete=models.CASCADE)
-    Rid = models.IntegerField()
-    grade = models.CharField(max_length=4)
-    sub1 = models.CharField(max_length=20)
-    sub2 = models.CharField(max_length=20)
-    sub3 = models.CharField(max_length=20)
-    marks1 = models.IntegerField()
-    marks2 = models.IntegerField()
-    marks3 = models.IntegerField()
-
-
-
-    render(request,'uploadresult.html')
 
 class IndexView(TemplateView):
     template_name = 'teacher/index.html'
