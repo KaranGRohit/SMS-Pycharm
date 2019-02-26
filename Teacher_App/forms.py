a07d78df8612789a99a666148416a6f1b82b3333
KaranGRohit <karanceddu@gmail.com>
2019-02-26 17:27:51 +0530

    changes in  new views to student app timetable view
    Signed-off-by: KaranGRohit <karanceddu@gmail.com>

--- Teacher_App/forms.py
+++ Teacher_App/forms.py
@@ -1,4 +1,21 @@
 from django import forms
-'''
-class StudentForm(forms.Form):
-   '''
+
+from .models import models
+
+class ResultForm(forms.Form):
+    Rid = forms.IntegerField()
+
+
+
+
+
+
+    Rid = models.IntegerField()
+    grade = models.CharField(max_length=4)
+    sub1 = models.CharField(max_length=20)
+    sub2 = models.CharField(max_length=20)
+    sub3 = models.CharField(max_length=20)
+    marks1 = models.IntegerField()
+    marks2 = models.IntegerField()
+    marks3 = models.IntegerField()
+
